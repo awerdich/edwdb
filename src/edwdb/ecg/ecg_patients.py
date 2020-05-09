9398bd735a6444e47dcda1e7a13c994e96f6f93a
awerdich <awerdich@gmail.com>
2020-05-09 15:26:29 -0400

    Small update.

--- src/edwdb/ecg/ecg_patients.py
+++ src/edwdb/ecg/ecg_patients.py
@@ -2,6 +2,9 @@ import os
 import pandas as pd
 from dotenv import load_dotenv
 
+# Custom imports
+from ehr import edw
+
 #%% Files and directories
 data_root = os.path.normpath('/mnt/obi0/andreas/data/ecg')
 parquet_dir = os.path.join(data_root, 'parquet')
@@ -15,7 +18,16 @@ load_dotenv(dotenv_file)
 df = pd.read_parquet(mgh_meta_file)
 print(f'Loaded meta data for {len(df.file.unique())} ecgs from {len(df.PatientID.unique())} mrns.')
 # rename mrn and select only the important columns
-ecg_cols = ['PatientID', 'TestDate', 'TestTime', 'file']
-df_ecg = df[ecg_cols].rename(columns={'PatientID': 'mrn'}).reset_index(drop=True)
-df_ecg_file = os.path.join(parquet_dir, 'MGH_RAW_all_mrn.parquet')
-df_ecg.to_parquet(df_ecg_file)
+df = df.rename(columns={'PatientID': 'mrn'}).reset_index(drop=True)
+
+#%% get PatientIDs
+# get a few mrns for testing
+mrn_list = list(df.mrn.unique()[0:10])
+
+# Epic class for pulling data from edw
+epic = edw.Epic(edw_user = os.environ['EDW_USER'],
+                edw_password = os.environ['EDW_PASSWORD'],
+                db = 'PHS',
+                out_dir = data_root,
+                dataset_name = 'ecg')
+
