6a4c8ebdeaf160f3d8dce97904f4cc197cf13432
awerdich <awerdich@gmail.com>
2020-05-16 11:34:03 -0400

    Merging meds from db and ref table only on MedicationID now.

--- src/edwdb/ecg/ecg_patients.py
+++ src/edwdb/ecg/ecg_patients.py
@@ -3,7 +3,6 @@ import typing
 import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
-import glob
 
 random_state = 123 # Random state for pd sampling.
 
@@ -16,6 +15,7 @@ pd.set_option('display.width', 1000)
 #%% Files and directories
 data_root = os.path.normpath('/mnt/obi0/andreas/data/ecg')
 parquet_dir = os.path.join(data_root, 'parquet')
+meta_dir = os.path.join(parquet_dir, 'betablockerdata_parts')
 dotenv_file = os.path.normpath('/mnt/obi0/andreas/config/credentials.env')
 load_dotenv(dotenv_file)
 
@@ -62,13 +62,12 @@ def concat_rx(df):
     df_concat = df_concat.merge(df_rest, on='file', how='left')
     return df_concat
 
-def ecg_in_TestOrderInterval(idlist, ecg_df, med_df, medid_target_list, PharmaceuticalClassDSC, TherapeuticClassDSC):
+def ecg_in_TestOrderInterval(idlist, ecg_df, med_df, medid_target_list):
 
     # ECGs with medications in target list
     ecg_med_target_list = []
     # All other ECGs with medication lists
-    ecg_med_nontarget_list_short = []
-    ecg_med_nontarget_list_long = []
+    ecg_med_nontarget_list = []
 
     # Go through each patient and combine ECGs with medications
     for p, mrn in enumerate(idlist):
@@ -85,39 +84,36 @@ def ecg_in_TestOrderInterval(idlist, ecg_df, med_df, medid_target_list, Pharmace
             mrn_med_target = mrn_med[mrn_med.MedicationID.isin(medid_target_list)]
             if mrn_med_target.shape[0] > 0:
                 # Generate OrderInterval from start/end dates
-                mrn_med_target = mrn_med_target.assign(OrderInterval = mrn_med_target.apply(med_interval, axis = 1))
+                mrn_med_target = mrn_med_target.assign(OrderInterval=mrn_med_target.apply(med_interval, axis = 1))
 
                 # Combine TestDate and TestTime columns to generate a TimeStamp
-                mrn_ecg = mrn_ecg.assign(TestTimestamp = pd.to_datetime(mrn_ecg['TestDate'].apply(str)+' '+mrn_ecg['TestTime']))
+                mrn_ecg = mrn_ecg.assign(TestTimestamp=pd.to_datetime(mrn_ecg['TestDate'].apply(str)+' '+mrn_ecg['TestTime']))
 
                 # Merge med on ecg table and determine if TestTimesamp is in Orderinterval for each ECG and each Medication
-                ecg_med = mrn_ecg.merge(mrn_med_target, on = 'MRN', how='left')
-                ecg_med = ecg_med.assign(TestInOrderInterval = ecg_med.\
+                ecg_med = mrn_ecg.merge(mrn_med_target, on='MRN', how='left')
+                ecg_med = ecg_med.assign(TestInOrderInterval=ecg_med.\
                                          apply(lambda s: s['TestTimestamp'] in s['OrderInterval'], axis = 1))
 
                 # We are only interested in those ECGs that occurred within the OrderInterval
                 ecg_med_int = ecg_med[ecg_med.TestInOrderInterval==True]
 
                 if ecg_med_int.shape[0] > 0:
-                    ecg_med_int_concat = concat_rx(ecg_med_int)
-                    ecg_med_int_concat = ecg_med_int_concat.assign(PharmaceuticalClassDSC=PharmaceuticalClassDSC,
-                                                                   TherapeuticClassDSC=TherapeuticClassDSC)
-                    ecg_med_target_list.append(ecg_med_int_concat)
+                    #ecg_med_int_concat = concat_rx(ecg_med_int)
+                    ecg_med_target_list.append(ecg_med_int)
             else:
                 # If there are no medications in target list, save ECGs and meds in a separage dataframe
                 ecg_med = mrn_ecg.merge(mrn_med, on=['MRN'], how='left')
-                ecg_med_nontarget_list_long.append(ecg_med)
+                ecg_med_nontarget_list.append(ecg_med)
 
-                ecg_med_concat = concat_rx(ecg_med).drop_duplicates(subset=['MRN', 'file']).reset_index(drop=True)
+                #ecg_med_concat = concat_rx(ecg_med).drop_duplicates(subset=['MRN', 'file']).reset_index(drop=True)
                 # We do not need the dates any more (because we concatenated the meds)
-                ecg_med_concat = ecg_med_concat.drop(columns=['OrderStartDTS', 'OrderEndDTS'])
-                ecg_med_nontarget_list_short.append(ecg_med_concat)
+                #ecg_med_concat = ecg_med_concat.drop(columns=['OrderStartDTS', 'OrderEndDTS'])
+                #ecg_med_nontarget_list_short.append(ecg_med_concat)
 
     target_df = pd.concat(ecg_med_target_list, ignore_index=True).reset_index(drop=False)
-    nontarget_df_short = pd.concat(ecg_med_nontarget_list_short, ignore_index=True).reset_index(drop=False)
-    nontarget_df_long = pd.concat(ecg_med_nontarget_list_long, ignore_index=True).reset_index(drop=False)
+    nontarget_df = pd.concat(ecg_med_nontarget_list, ignore_index=True).reset_index(drop=False)
 
-    return target_df, nontarget_df_short, nontarget_df_long
+    return target_df, nontarget_df
 
 #%% Query functions
 
@@ -212,65 +208,33 @@ for chunk, idlist in enumerate(mrn_chunk_list):
     print(f'Querying database for {len(idlist)} mrns.')
 
     # Database query for medications
-    #idlist=idlist[0:300]
+    # idlist=idlist[0:300]
     med_df = medications(idlist)
     med_df = med_df.dropna(subset=['MGHMRN', 'OrderStartDTS', 'MedicationID']).\
         astype({'MGHMRN': 'int', 'MedicationID': 'int'}).\
-        rename(columns={'MGHMRN': 'MRN'}).reset_index(drop=True)
+        rename(columns={'MGHMRN': 'MRN', 'MedicationDSC': 'MedicationDSCDB'}).reset_index(drop=True)
 
     print(f'Completed. Found {len(med_df.PatientID.unique())} mrns with medication data.')
 
-    target_df, nontarget_df_short, nontarget_df_long = ecg_in_TestOrderInterval(idlist=idlist,
-                                                                                ecg_df=ecg_df,
-                                                                                med_df=med_df,
-                                                                                medid_target_list=medid_target_list,
-                                                                                PharmaceuticalClassDSC=PharmaceuticalClassDSC,
-                                                                                TherapeuticClassDSC=TherapeuticClassDSC)
-    # Add Medication categories to nontarget_df
-    nontarget_df_ref = nontarget_df_long.merge(ref_med, on=['MedicationID', 'MedicationDSC'], how = 'left')
+    target_df, nontarget_df = ecg_in_TestOrderInterval(idlist=idlist,
+                                                       ecg_df=ecg_df,
+                                                       med_df=med_df,
+                                                       medid_target_list=medid_target_list)
+    # Add Medication categories
+    # We have two MedicationDSC columns. Drop the one from the DB
+    target_df_ref = target_df.merge(ref_med, on='MedicationID', how='left').\
+        drop(columns=['MedicationDSCDB']).drop_duplicates().reset_index(drop=True)
+    nontarget_df_ref = nontarget_df.merge(ref_med, on='MedicationID', how='left').\
+        drop(columns=['MedicationDSCDB']).drop_duplicates().reset_index(drop=True)
 
     # Save part file
     base_part_name = os.path.basename(ecg_meta_file).split('.')[0] + '_'+str(chunk).zfill(mag) + '.parquet'
     target_part_name = 'beta_' + base_part_name
-    nontarget_part_name_short = 'other_' + base_part_name
-    nontarget_part_name_long = 'other_long_' + base_part_name
+    nontarget_part_name = 'other_' + base_part_name
 
     # Need to convert OrderInterval column to string
-    target_df = target_df.astype({'OrderInterval': 'str'})
+    target_df_ref = target_df_ref.astype({'OrderInterval': 'str'})
 
     # Save data
-    target_df.to_parquet(os.path.join(data_root, target_part_name))
-    nontarget_df_short.to_parquet(os.path.join(data_root, nontarget_part_name_short))
-    nontarget_df_ref.to_parquet(os.path.join(data_root, nontarget_part_name_long))
-
-#%% Load sample data
-
-def load_files(data_root):
-    target_files = glob.glob(os.path.join(data_root, 'beta_MGH_RAW_FROM2001*.parquet'))
-    nontarget_short_files = glob.glob(os.path.join(data_root, 'other_MGH_RAW_FROM2001*.parquet'))
-    nontarget_long_files = glob.glob(os.path.join(data_root, 'other_long_MGH_RAW_FROM2001*.parquet'))
-    print(f'Number of data files found: {len(target_files)}')
-    target_df_list = []
-    nontarget_short_df_list = []
-    nontarget_long_df_list = []
-    for f in range(len(target_files)):
-        print(f'Reading data set {f+1} of {len(target_files)}')
-        target_df_list.append(pd.read_parquet(target_files[f]))
-        nontarget_short_df_list.append(pd.read_parquet(nontarget_short_files[f]))
-        nontarget_long_df_list.append(pd.read_parquet(nontarget_long_files[f]))
-
-    target_df = pd.concat(target_df_list, ignore_index=True).reset_index(drop=True)
-    nontarget_short_df = pd.concat(nontarget_short_df_list, ignore_index=True).reset_index(drop=True)
-    nontarget_long_df = pd.concat(nontarget_long_df_list, ignore_index=True).reset_index(drop=True)
-
-    return target_df, nontarget_short_df, nontarget_long_df
-
-target_df, nontarget_short_df, nontarget_long_df = load_files(data_root)
-
-#%% Printing some data
-print(f'Found {len(target_df.file.unique())} ecgs from {len(target_df.MRN.unique())} mrns in target group.')
-print(f'Found {len(nontarget_short_df.file.unique())} ecgs from {len(nontarget_short_df.MRN.unique())} mrns in nontarget group.')
-
-
-
-
+    target_df_ref.to_parquet(os.path.join(meta_dir, target_part_name))
+    nontarget_df_ref.to_parquet(os.path.join(meta_dir, nontarget_part_name))
